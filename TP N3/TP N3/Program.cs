86551603876171003998dd7c9470deb822915d97
lucianocolin <lucianocolin99@gmail.com>
2023-06-06 22:25:19 -0300

    first exc

--- TP N3/TP N3/Program.cs
+++ TP N3/TP N3/Program.cs
@@ -10,15 +10,20 @@ namespace TP_N3
     {
         static void Main(string[] args)
         {
-            string nombre;
+            //EJERCICIO 1:  
+            int[] vector = { 20, 32, -10, 54, 102 };
+            int acumulador = 0;
+            int sumaTotal = 0;
 
             Console.Clear();
+            for (int i = 0; i < vector.Length; i++)
+            {
+                acumulador += vector[i];
+                sumaTotal = acumulador;
+            }
 
-            Console.WriteLine("Ingrese su nombre: ");
-            nombre = Console.ReadLine();
-
-            Console.WriteLine($"BIENVENIDO {nombre}");
-
+            Console.WriteLine($"La suma del vector es igual a: {sumaTotal}");
+            Console.WriteLine("EL PROGRAMA FINALIZÓ");
             Console.ReadKey();
         }
     }
